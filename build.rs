76ea180bc11e31a730592c1f3b1190865be797d5
ajz34 <ajz34@outlook.com>
2024-12-08 10:10:22 +0800

    update build.rs to accept LD_LIBRARY_PATH search dylib

--- build.rs
+++ build.rs
@@ -1,21 +1,60 @@
-use std::env;
+use std::{error::Error, path::PathBuf};
+
+/// Generate link search paths from a list of paths.
+///
+/// This allows paths like `/path/to/lib1:/path/to/lib2` to be split into individual paths.
+fn generate_link_search_paths(paths: &[Result<String, impl Error + Clone>]) -> Vec<String> {
+    paths
+        .iter()
+        .map(|path| {
+            path.clone()
+                .unwrap_or_default()
+                .split(":")
+                .map(|path| path.to_string())
+                .collect::<Vec<_>>()
+        })
+        .into_iter()
+        .flatten()
+        .filter(|path| !path.is_empty())
+        .collect::<Vec<_>>()
+}
+
+/// Check if the library is found in the given paths.
+fn check_library_found(
+    lib_name: &str,
+    lib_paths: &[String],
+    lib_extension: &[String],
+) -> Option<String> {
+    for path in lib_paths {
+        for ext in lib_extension {
+            let lib_path = PathBuf::from(&path).join(format!("lib{}.{}", lib_name, ext));
+            if lib_path.exists() {
+                return Some(lib_path.to_string_lossy().to_string());
+            }
+        }
+    }
+    return None;
+}
 
 fn main() {
-    let external_dir = if let Ok(external_dir) = env::var("REST_EXT_DIR") {
-        external_dir
-    } else {
-        "".to_string()
-    };
+    // search dirs
+    for key in ["REST_EXT_DIR", "LD_LIBRARY_PATH"].iter() {
+        println!("cargo:rerun-if-env-changed={}", key);
+    }
+    let lib_paths = generate_link_search_paths(&[
+        std::env::var("REST_EXT_DIR"),
+        std::env::var("LD_LIBRARY_PATH"),
+    ]);
 
-    let library_names = ["openblas", "xc", "rest2fch"];
-    library_names.iter().for_each(|name| {
-        println!("cargo:rustc-link-lib={}", *name);
-    });
-    let library_path = [std::fs::canonicalize(&external_dir).unwrap()];
-    library_path.iter().for_each(|path| {
-        println!(
-            "cargo:rustc-link-search={}",
-            env::join_paths(&[path]).unwrap().to_str().unwrap()
-        )
-    });
+    // find libopenblas.so, libxc.so and link them
+    for library_name in ["openblas", "xc"] {
+        if let Some(path) = check_library_found(&library_name, &lib_paths, &["so".to_string()]) {
+            let path = std::fs::canonicalize(path).unwrap();
+            let path = path.parent().unwrap().display();
+            println!("cargo:rustc-link-search=native={}", path);
+            println!("cargo:rustc-link-lib={}", library_name);
+        } else {
+            panic!("lib{}.so not found!", library_name)
+        }
+    }
 }
